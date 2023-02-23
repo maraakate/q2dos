2e8ffe2bf51f98968a5683943a5bdf8903660eb5
Ozkan Sezer <sezeroz@gmail.com>
2023-02-23 17:40:10 +0300

    coop/tsmod: unix builds don't need local regex.[ch]

--- coop/tsmod/g_local.h
+++ coop/tsmod/g_local.h
@@ -36,7 +36,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "game.h"
 
 #include <ctype.h>
+#ifndef _WIN32
+#include <regex.h>
+#else
 #include "regex.h"
+#endif
 
 //*** UPDATE START ***
 #define PRIVATE_COMMANDS  8
