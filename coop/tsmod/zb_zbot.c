92508d493bfca74460479e6b1249a717e64c0741
Ozkan Sezer <sezeroz@gmail.com>
2023-02-23 17:40:10 +0300

    coop/tsmod: fix misuse of addressof operator on arrays in scanf calls.

--- coop/tsmod/zb_zbot.c
+++ coop/tsmod/zb_zbot.c
@@ -1505,7 +1505,7 @@ void Read_Admin_cfg(void)
 	i = 0;
 	while ((!feof(f)) && (i < MAX_ADMINS))
 	{
-		fscanf(f, "%s %s %d", &admin_pass[i].name, &admin_pass[i].password, &admin_pass[i].level);
+		fscanf(f, "%s %s %d", admin_pass[i].name, admin_pass[i].password, &admin_pass[i].level);
 		i++;
 	}
 	if (!admin_pass[i].level)
@@ -1531,7 +1531,7 @@ file2:;
 	i = 0;
 	while ((!feof(f)) && (i < MAX_ADMINS))
 	{
-		fscanf(f, "%s %s %d", &q2a_bypass_pass[i].name, &q2a_bypass_pass[i].password, &q2a_bypass_pass[i].level);
+		fscanf(f, "%s %s %d", q2a_bypass_pass[i].name, q2a_bypass_pass[i].password, &q2a_bypass_pass[i].level);
 		i++;
 	}
 	if (!q2a_bypass_pass[i].level)
@@ -2161,18 +2161,18 @@ void whois_read_file(void)
 	{
 		fscanf(f, "%i %s %s %s %s %s %s %s %s %s %s %s %s",
 			&whois_details[WHOIS_COUNT].id,
-			&whois_details[WHOIS_COUNT].ip,
-			&whois_details[WHOIS_COUNT].seen,
-			&whois_details[WHOIS_COUNT].dyn[0].name,
-			&whois_details[WHOIS_COUNT].dyn[1].name,
-			&whois_details[WHOIS_COUNT].dyn[2].name,
-			&whois_details[WHOIS_COUNT].dyn[3].name,
-			&whois_details[WHOIS_COUNT].dyn[4].name,
-			&whois_details[WHOIS_COUNT].dyn[5].name,
-			&whois_details[WHOIS_COUNT].dyn[6].name,
-			&whois_details[WHOIS_COUNT].dyn[7].name,
-			&whois_details[WHOIS_COUNT].dyn[8].name,
-			&whois_details[WHOIS_COUNT].dyn[9].name);
+			whois_details[WHOIS_COUNT].ip,
+			whois_details[WHOIS_COUNT].seen,
+			whois_details[WHOIS_COUNT].dyn[0].name,
+			whois_details[WHOIS_COUNT].dyn[1].name,
+			whois_details[WHOIS_COUNT].dyn[2].name,
+			whois_details[WHOIS_COUNT].dyn[3].name,
+			whois_details[WHOIS_COUNT].dyn[4].name,
+			whois_details[WHOIS_COUNT].dyn[5].name,
+			whois_details[WHOIS_COUNT].dyn[6].name,
+			whois_details[WHOIS_COUNT].dyn[7].name,
+			whois_details[WHOIS_COUNT].dyn[8].name,
+			whois_details[WHOIS_COUNT].dyn[9].name);
 
 		//convert all ÿ back to spaces
 		temp_len = strlen(whois_details[WHOIS_COUNT].ip);
